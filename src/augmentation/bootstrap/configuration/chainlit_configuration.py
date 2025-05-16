d5d37dd3ecb5a720e1e9b0dca75a83d09ac901e9
piotr.kalota <piotr.kalota.dev@gmail.com>
2025-05-16 10:30:44 +0200

    Update disclaimer text

--- src/augmentation/bootstrap/configuration/chainlit_configuration.py
+++ src/augmentation/bootstrap/configuration/chainlit_configuration.py
@@ -16,7 +16,7 @@ class ChainlitConfiguration(BaseConfiguration):
         description="Title of the disclaimer message to be displayed.",
     )
     disclaimer_text: str = Field(
-        "This content is AI-generated and may contain inaccuracies. Please verify any critical information independently. More details can be found in readme.",
+        "This content is AI-generated and may contain inaccuracies. Please verify any critical information independently. Additional details, including imprint and privacy policy information, can be found in the **Readme file located at he top-right corner of the page.**",
         description="Disclaimer text to be displayed to users.",
     )
     welcome_message: str = Field(
